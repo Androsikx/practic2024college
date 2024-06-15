52abb1626a2561937e3f3f482c6e671de1af9c5d
andros <116491925+Androsikx@users.noreply.github.com>
2024-06-15 03:10:42 +0000

    rename-href

--- portfolio.php
+++ portfolio.php
@@ -1,11 +1,10 @@
-<!-- pages/portfolio.php -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Портфоліо | Клінінгова компанія</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles1.css">
 </head>
 <body>
     <header>
