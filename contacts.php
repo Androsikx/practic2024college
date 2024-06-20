9b0be2adb9ed74ff55ff4fbb2902c8a6c7077af8
andros <116491925+Androsikx@users.noreply.github.com>
2024-06-20 06:45:00 +0000

    edit review

--- contacts.php
+++ contacts.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Контакти | Клінінгова компанія</title>
-    <link rel="stylesheet" href="styles2.css">
+    <link rel="stylesheet" href="styles3.css">
 </head>
 <body>
     <header>
@@ -22,14 +22,20 @@
     
     <div class="container">
         <section>
+        <div class="contact">
             <h2>Робочий час:</h2>
-            <h2>Графік роботи: Пн-Пт 9:00 – 18:00</h2>
-            <h2>Субота: 10:00 – 15:00</h2>
-            <h2>Неділя: вихідний</h2>
-            <h2>М. Коломия</h2>
+            <p>Графік роботи: Пн-Пт 9:00 – 18:00</p>
+            <p>Субота: 10:00 – 15:00</p>
+            <p>Неділя: вихідний</p>
+            </div>
+            <div class="contact">
+            <h2>Місце положення ↓ </h2>
+            <p>М. Коломия Вул.Лисенка 32</p>
+            </div>
+            <div class="contact">
             <h2>↓ Способи зв'язку ↓</h2>
-            <h2>Пошта: CliningAndro@gmail.com</h2>
-            <h2>Тел: +380 (99) 303 92 71</h2>
+            <p>Пошта: CliningAndro@gmail.com</p>
+            <p>Тел: +380 (99) 303 92 71</p>
             <img src="123.gif" alt="Контакти GIF" class="contact-gif">
         </section>
     </div>
